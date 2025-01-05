f27bd0ef4a953c35f30520f1fd5c914f88204fcf
qAbdullajon <azikmelor7705@gmail.com>
2025-01-06 00:31:36 +0500

    responsivlik

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -36,41 +36,43 @@ const Home = ({ setUser }: any) => {
 
   return (
     <div className="w-full bg-white flex justify-center bg-gradient-to-r from-[#C58FC0] to-[#7F5698]">
-      <div className="py-8">
+      <div className="py-8 px-4 max-w-[480px] w-full flex flex-col items-center">
         <p className="text-white text-2xl pb-3 uppercase font-medium text-center">
           Marafon ishtirokchilari <br /> uchun maxsus taklif
         </p>
-        <div className="rounded-xl bg-white w-[450px] overflow-hidden relative">
-          <div className="bg-black w-full px-5 pt-2">
-            <div className="px-8 py-3 flex justify-between items-center z-20 relative">
-              <div className="w-6 h-6 bg-[#C7A2FF] rounded-full flex rotate-45 justify-center items-center">
-                <div className="border-b-[3.5px] border-r-[3.5px] border-black w-2 h-3"></div>
+        <div className="rounded-xl bg-white w-full overflow-hidden relative">
+          <div className="w-full bg-black flex justify-center">
+            <div className="w-full max-w-[400px] px-5 pt-2">
+              <div className="px-4 py-3 flex justify-between items-center z-20 relative">
+                <div className="w-6 h-6 bg-[#C7A2FF] rounded-full z-30 flex rotate-45 justify-center items-center">
+                  <div className="border-b-[3.5px] border-r-[3.5px] border-black w-2 h-3"></div>
+                </div>
+                <div className="bg-[#525057] w-6 h-6 rounded-full z-30 flex justify-center items-center">
+                  <div className="w-[10px] h-[10px] bg-black rounded-full"></div>
+                </div>
+                <div className="bg-[#525057] w-6 h-6 rounded-full z-30 flex justify-center items-center">
+                  <div className="w-[10px] h-[10px] bg-black rounded-full"></div>
+                </div>
+                <div className="w-[88%] h-[6px] bg-[#525057] absolute left-[17px] top-[21px] z-0"></div>
               </div>
-              <div className="bg-[#525057] w-6 h-6 rounded-full flex justify-center items-center">
-                <div className="w-[10px] h-[10px] bg-black rounded-full"></div>
+              <div className="flex justify-between relative bottom-2">
+                <div className="text-sm min-[450px]:text-[17px] relative right-1 text-center text-white">
+                  Malumot <br /> kiritish
+                </div>
+                <div className="text-sm min-[450px]:text-[17px] relative left-1 text-center text-white">
+                  Chekni <br /> kiritish
+                </div>
+                <div className="text-sm min-[450px]:text-[17px] relative left-2 text-center text-white">Yakunlash</div>
               </div>
-              <div className="bg-[#525057] w-6 h-6 rounded-full flex justify-center items-center">
-                <div className="w-[10px] h-[10px] bg-black rounded-full"></div>
-              </div>
-            </div>
-            <div className="w-[70%] h-[6px] bg-[#525057] absolute top-[29px] left-16 z-10"></div>
-            <div className="flex justify-between px-3 relative bottom-2">
-              <div className="text-[17px] text-center text-white">
-                Malumot <br /> kiritish
-              </div>
-              <div className="text-[17px] text-center text-white">
-                Chekni <br /> kiritish
-              </div>
-              <div className="text-[17px] text-center text-white">Yakunlash</div>
             </div>
           </div>
-          <div className="px-4 py-4">
+          <div className="px-3 py-4">
             <p className="bg-black text-white text-center text-xl font-bold rounded-lg py-2">Rus tili 40 kunda 5.0</p>
             <p className="text-xl pt-2 pb-3 text-center">
               <span className="font-semibold">Start: </span> 15-yanvar
             </p>
             <p className="text-xl font-semibold text-center">
-              Dilshoda Qurbonova bilan Rus tilini <br /> samarali o'rganing
+              Dilshoda Qurbonova bilan Rus tilini <br className="hidden min-[440px]:block" /> samarali o'rganing
             </p>
             <p className="text-sm text-center py-2 font-light">Hozirgi formani to'ldiring va bonusni qo'lga kiriting!</p>
           </div>
@@ -79,6 +81,7 @@ const Home = ({ setUser }: any) => {
             layout="vertical"
             style={{
               maxWidth: "400px",
+              paddingInline: "10px",
               borderRadius: "0",
               marginInline: "auto",
             }}
